93132ccfab5bec842caa268a64f3597383093844
Bryan Rafael Andia <player.b.96@hotmail.com>
2025-12-24 15:52:34 -0500

    quitar audience

--- app/src/main.js
+++ app/src/main.js
@@ -14,7 +14,6 @@ app.use(
     clientId: import.meta.env.VITE_AUTH0_CLIENT_ID,
     authorizationParams: {
       redirect_uri: import.meta.env.VITE_AUTH0_CALLBACK_URL,
-      audience: import.meta.env.VITE_AUTH0_AUDIENCE
     },
     cacheLocation: 'localstorage',
     useRefreshTokens: true
